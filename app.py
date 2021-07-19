562aa22803812f1f846e457eb437f90668e04de4
Rizze Dvonick <benidiktusvio@gmail.com>
2021-07-20 06:31:35 +0700

    Fix Heroku deploy

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import re
 from flask import Flask, request, Response
 import requests as req
 app = Flask(__name__)
@@ -8,7 +7,7 @@ app = Flask(__name__)
 @app.route("/json/<ip>", methods=["GET"])
 def fetchData(ip=None):
     if request.method == "GET":
-        query = ip if ip else request.remote_addr
+        query = ip if ip else request.headers["X-Forwarded-For"]
         res = req.get(f"http://ip-api.com/json/{query}?fields=status,message,continent,country,countryCode,regionName,city,zip,lat,lon,timezone,isp,org,as,mobile,proxy,query")
         return Response(res.text, mimetype="application/json")
 
